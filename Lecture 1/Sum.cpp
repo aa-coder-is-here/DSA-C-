bb1b5c1e445ef47c87742f53c2844268d87aaa20
Ali Arif <aliarif46807@gmail.com>
2025-03-26 13:06:58 +0500

    The challenge is done

---
+++ Lecture 1/Sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a , b , sum;
+    cout << "Enter the value of a & b: ";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "The Sum of two numbers are: " << sum << endl;
+
+    return 0;
+}
