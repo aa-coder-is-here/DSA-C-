18b71e3138df5f75c667612da677a14ba715df57
Ali Arif <aliarif46807@gmail.com>
2025-03-27 12:07:51 +0500

    Check the character is uppercase or lowercase by 2nd method

--- Lecture 2/Conditional/if else if/charCheck.cpp
+++ Lecture 2/Conditional/if else if/charCheck.cpp
@@ -11,4 +11,11 @@ int main(){
     }else if(n >= 97 && n < 122){
         cout << "You enter a lowercase character \n";
     }
+
+    // Second method
+    // if(character >= 'a' && character < 'z'){
+    //     cout << "You enter a uppercase character \n";
+    // }else if(character >= 'A' && character < 'Z'){
+    //     cout << "You enter a lowercase character \n";
+    // }
 }
