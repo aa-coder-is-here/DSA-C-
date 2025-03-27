389c504271604bda7e4a658c3e011cb77ce8fff7
Ali Arif <aliarif46807@gmail.com>
2025-03-27 12:46:02 +0500

    Sum of odd numbers

---
+++ Lecture 2/Loops/for/SumOfOdd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int sum = 0 , n;
+    cout << "Enter the number: ";
+    cin >> n;
+    for(int i = 1 ; i<= n;i+=2){
+        sum += i;
+    }
+    cout << "The sum of odd numbers are: " << sum;
+    
+    return 0;
+}
