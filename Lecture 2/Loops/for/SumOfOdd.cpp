b663ff2c2f82b8b9dbe38a8a6d1b4ea389299428
Ali Arif <aliarif46807@gmail.com>
2025-03-27 15:17:06 +0500

    Compiler issue f* up the time

--- Lecture 2/Loops/for/SumOfOdd.cpp
+++ Lecture 2/Loops/for/SumOfOdd.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main(){
     int sum = 0 , n;
-    cout << "Enter the number: ";
+    // cout << "Enter the number: ";
     cin >> n;
     for(int i = 1 ; i<= n;i+=2){
         if(i%2 != 0)
           sum += i;
     }
-    cout << "The sum of odd numbers are: " << sum;
+    // cout << "The sum of odd numbers are: " << sum;
     
     return 0;
 }
