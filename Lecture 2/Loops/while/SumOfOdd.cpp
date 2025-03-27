b83d1f076f1a16a6c595259401e144ef089865ea
Ali Arif <aliarif46807@gmail.com>
2025-03-27 12:51:54 +0500

    The same question with while

---
+++ Lecture 2/Loops/while/SumOfOdd.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int sum = 0 , n , i = 1;
+    cout << "Enter the number: ";
+    cin >> n;
+    while(i <= n){
+        if(i%2 != 0){
+            sum += i;
+            i++;
+        }else{
+            i++;
+            continue;
+        }
+    }
+    cout << "The sum is = " << sum;
+    
+    return 0;
+}
