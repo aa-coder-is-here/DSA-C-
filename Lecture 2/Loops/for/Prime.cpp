a07b250b8be91fca2b8dbef0dc0e9eaeba0c59b1
Ali Arif <aliarif46807@gmail.com>
2025-03-27 15:30:53 +0500

    The Prime number checker is done

--- Lecture 2/Loops/for/Prime.cpp
+++ Lecture 2/Loops/for/Prime.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "";
+    int num;
+    bool isPrime = true;
+    cout << "Enter a number: ";
+    cin >> num;
+    for(int i = 2; i < num ; i++){
+       if(num%i == 0){
+          isPrime = false;
+          break;
+       }
+    }
+    cout << (isPrime == true ? "The Number is Prime" : "The Number is not Prime");
+
+    return 0;
 }
